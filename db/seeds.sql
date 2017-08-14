17ebc99cce87270a931b0bdbab0fcb37f56fc780
Andy Lushman <andylushman@gmail.com>
2017-08-14 14:36:01 -0600

    Created db based on schema.sql and seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burger (name) VALUES ("Turkey");
-INSERT INTO burger (name) VALUES ("Avocado");
-INSERT INTO burger (name) VALUES ("Salmon");
+INSERT INTO burger (burger_name) VALUES ("Turkey");
+INSERT INTO burger (burger_name) VALUES ("Avocado");
+INSERT INTO burger (burger_name) VALUES ("Salmon");
